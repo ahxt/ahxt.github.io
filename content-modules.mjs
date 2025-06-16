ad1880212cf75b6799a3a23c4e193f5cfa18964f
Xiaotian Han <xhan@case.edu>
2025-06-16 16:24:24 -0400

    update

---
+++ content-modules.mjs
@@ -0,0 +1,11 @@
+
+export default new Map([
+["content/blog/2024-10-20-attention.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2024-10-20-attention.typ&astroContentModuleFlag=true")],
+["content/blog/2024-12-12-celoss.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2024-12-12-celoss.typ&astroContentModuleFlag=true")],
+["content/blog/2024-10-19-softmax.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2024-10-19-softmax.typ&astroContentModuleFlag=true")],
+["content/blog/2024-11-20-graph-convolution-mixup.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2024-11-20-graph-convolution-mixup.typ&astroContentModuleFlag=true")],
+["content/blog/2024-12-30-inference-time-scaling-exp.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2024-12-30-inference-time-scaling-exp.typ&astroContentModuleFlag=true")],
+["content/blog/2025-01-22-optimizer.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2025-01-22-optimizer.typ&astroContentModuleFlag=true")],
+["content/blog/2025-03-18-rope.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2025-03-18-rope.typ&astroContentModuleFlag=true")],
+["content/blog/2025-01-21-llm-tech-reoport.typ", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2F2025-01-21-llm-tech-reoport.typ&astroContentModuleFlag=true")]]);
+		
